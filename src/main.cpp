8dab1a91d1d632a2a52497500389e8cd5d148a0c
RamiAwar <rami.awar.ra@gmail.com>
2018-12-03 07:56:10 +0200

    - Added EllipticCurve.h, not compiling. Strange errors. Variable declaration understood as a type. Integer literals too large to be saved except they're not. Check that they exist on other machines as well.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include "main.h"
 
+#include "EllipticCurve.h"
+
 int main() {
+<<<<<<< HEAD
     std::cout << a << std::endl;
     return 0;
+=======
+	
+	EllipticCurve c = StandardEllipticCurves.find("secp256k1")->second;
+	
+	std::cout << c.p << std::endl;
+	return 0;
+>>>>>>> - Added EllipticCurve.h, not compiling.
 }
