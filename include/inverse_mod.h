0750cc4056d60d7f329907fb39ccc2a6404535ca
Elie <eliemaamary17@gmail.com>
2018-12-04 04:33:09 +0200

    merge conflict

--- include/inverse_mod.h
+++
@@ -1,5 +0,0 @@
-/**
- * Returns the multiplicative inverse of k mod p
- *
- */
-int inverse_mod(int k , int p);
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,6 @@
 #include <KeyGen.h>
 
 int main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    std::cout << a << std::endl;
-    return 0;
-=======
-	
-	EllipticCurve c = StandardEllipticCurves.find("secp256k1")->second;
-	
-	std::cout << c.p << std::endl;
-=======
 	EccCalculator* eccCalculator = new EccCalculator(secp192k1);
 	KeyGen* keyGen = new KeyGen(eccCalculator);
 	keyGen->GenerateKeyPair();	
@@ -21,7 +11,5 @@ int main() {
 	cpp_int privKey = keyGen->GetPrivateKey();
 	std::cout << "Private key: " << privKey << std::endl;
 	std::cout << "Public key: " << pubKey << std::endl;
->>>>>>> add public and private key generation
 	return 0;
->>>>>>> - Added EllipticCurve.h, not compiling.
 }
